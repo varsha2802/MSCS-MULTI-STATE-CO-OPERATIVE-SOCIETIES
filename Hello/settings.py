803543a2be7030fb3aadb0e10535a9a4da86090d
Varsha <86241924+varsha2802@users.noreply.github.com>
2023-06-23 19:57:40 +0530

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4@mawfr$*chks3%(5*w_f@5x3(u(%gpqd5^dhy)!3pc+&bu5#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
